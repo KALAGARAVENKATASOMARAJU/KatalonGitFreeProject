e239087868f15ba81c2eafbdcece234695c06891
KALAGARAVENKATASOMARAJU <113903365+KALAGARAVENKATASOMARAJU@users.noreply.github.com>
2022-09-21 12:43:14 +0530

    My First Commit

---
+++ Test Suites/Imported from Selenium IDE Scripts/New Test Suite (1).ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>New Test Suite (1)</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>0</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>c1e744ba-6b7e-41b1-9d70-4f605c62dc79</testSuiteGuid>
+</TestSuiteEntity>
---
+++ Test Suites/Imported from Selenium IDE Scripts/New Test Suite.ts
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>New Test Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>0</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>10da1207-360c-4b24-9504-56e2bacc14a8</testSuiteGuid>
+</TestSuiteEntity>
